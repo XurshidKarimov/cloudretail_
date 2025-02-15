7dccc07c512492535c8fe82e895dcb79fcc55403
Xurshid <88909414+XurshidKarimov@users.noreply.github.com>
2025-02-15 22:06:19 +0500

    set up lint

---
+++ packages/frontend/eslint.config.mjs
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: ['*/**.{js, mjs, cjs}'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+);
